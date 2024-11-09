17fe5d0c93d07892a529e653d36628457ed571d5
Carlo <60157120+carlocarlen@users.noreply.github.com>
2024-11-09 10:16:35 +0100

    Refactor Board: magic numbers

--- java/src/main/java/Board.java
+++ java/src/main/java/Board.java
@@ -3,13 +3,16 @@ import java.util.List;
 
 public class Board
 {
+    private static final Integer ROW_LENGTH = 3;
+    private static final Integer COLUMN_LENGTH = 3;
+
     private List<Tile> _plays = new ArrayList<>();
 
     public Board()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < ROW_LENGTH; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < COLUMN_LENGTH; j++)
             {
                 Tile tile = new Tile();
                 tile.X = i;
